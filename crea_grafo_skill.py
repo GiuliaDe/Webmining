2b8e310da22f30ce7bf02e1fde459a2ecdd84d90
massimotosato <tosatomassimo89@gmail.com>
2017-07-12 11:18:28 +0200

    grafo skill bambolina

--- crea_grafo_skill.py
+++ crea_grafo_skill.py
@@ -1,8 +1,12 @@
 from igraph import *
 
-vlist = []
-elist= []
+vlist = range(10)
+elist= [(1,5),(1,6),(1,7),(1,8),(1,4),(2,5),(2,7),(2,9),(3,4),(3,6),(3,9),(0,5),(0,6),(0,7),(0,8)]
 
-g = Graph()
-g.add_vertices(10)
-# g.add_edges()
+# g = Graph()
+g = Graph(vertex_attrs={"label":vlist}, edges=elist, directed=True)
+# g.add_vertices(10)
+# g.add_edges(elist)
+
+plot(g)
+
